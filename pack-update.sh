2d8abcfd025ade6015ecdb498f7b3357028307ee
Michael Belyaev <usrsse2@me.com>
2023-03-05 00:14:01 +0300

    Add scripts to unpack and pack IMAGEWTY update images

---
+++ pack-update.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -e
+[ $# -ne 1 ] && { 
+	echo "Usage:"
+	echo "    $0 update_file.img"
+	echo "        (WITHOUT .dump)"
+	echo "        It packs update_file.img.dump into update_file.img, overwriting the destination file if it already exists."
+	exit 1 
+}
+
+UPDATE_FILE=$1
+rm "$UPDATE_FILE"
+
+echo "Packing $UPDATE_FILE.dump/data_udisk into MINFS image $UPDATE_FILE.dump/data_udisk.fex"
+./bin/minfs make "$UPDATE_FILE.dump/data_udisk" "$UPDATE_FILE.dump/data_udisk.fex" "$UPDATE_FILE.dump/data_udisk/rootfs_ini.tmp"
+
+echo "Updating checksum of $UPDATE_FILE.dump/data_udisk.fex in $UPDATE_FILE.dump/Vdata_udisk.fex"
+./bin/add_checksum "$UPDATE_FILE.dump/data_udisk.fex" "$UPDATE_FILE.dump/Vdata_udisk.fex"
+
+echo "Packing $UPDATE_FILE.dump into update image $UPDATE_FILE"
+./bin/awimage -v "$UPDATE_FILE.dump"
---
+++ unpack-update.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -e
+
+[ $# -ne 1 ] && { 
+	echo "Usage:"
+	echo "    $0 update_file.img"
+	echo "        It unpacks update_file.img into update_file.img.dump, removing the destination directory if it already exists."
+	exit 1 
+}
+
+UPDATE_FILE=$1
+
+echo "Unpacking update image $UPDATE_FILE into $UPDATE_FILE.dump"
+rm -Rf "$UPDATE_FILE.dump"
+./bin/awimage -v "$UPDATE_FILE"
+
+echo "Unpacking MINFS image $UPDATE_FILE.dump/data_udisk.fex into $UPDATE_FILE.dump/data_udisk"
+mkdir "$UPDATE_FILE.dump/data_udisk"
+./bin/minfs dump "$UPDATE_FILE.dump/data_udisk.fex" "$UPDATE_FILE.dump/data_udisk"
+
