c5d0460889bde167e85b7f3d9433d7f1ecadd9df
Michael Belyaev <usrsse2@me.com>
2023-03-12 23:40:10 +0300

    Run unpack-update from any directory

--- unpack-update.sh
+++ unpack-update.sh
@@ -7,15 +7,15 @@ set -e
 	echo "        It unpacks update_file.img into update_file.img.dump, removing the destination directory if it already exists."
 	exit 1 
 }
-cd -- "$(dirname -- "$0")"
+SCRIPT_DIR="$(dirname -- "$0")"
 
 UPDATE_FILE=$1
 
 echo "Unpacking update image $UPDATE_FILE into $UPDATE_FILE.dump"
 rm -Rf "$UPDATE_FILE.dump"
-./bin/awimage -v "$UPDATE_FILE"
+"$SCRIPT_DIR"/bin/awimage -v "$UPDATE_FILE"
 
 echo "Unpacking MINFS image $UPDATE_FILE.dump/data_udisk.fex into $UPDATE_FILE.dump/data_udisk"
 mkdir "$UPDATE_FILE.dump/data_udisk"
-./bin/minfs dump "$UPDATE_FILE.dump/data_udisk.fex" "$UPDATE_FILE.dump/data_udisk"
+"$SCRIPT_DIR"/bin/minfs dump "$UPDATE_FILE.dump/data_udisk.fex" "$UPDATE_FILE.dump/data_udisk"
 
